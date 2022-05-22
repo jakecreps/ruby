a8d24d5cab6b9ae20d7a45cd58b8bddd4aa998a0
Jake Creps <crepschrome@gmail.com>
2022-05-22 17:41:52 -0400

    Add files via upload

---
+++ ruby.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import sys
+import argparse
+from colorama import Fore, Back, Style, init
+import csv
+import os.path
+from bitchute import *
+from rumble import *
+from youtube import *
+
+init(autoreset=True)
+
+earl = Fore.RED + r"""
+   ▄████████ ███    █▄  ▀█████████▄  ▄██   ▄
+  ███    ███ ███    ███   ███    ███ ███   ██▄
+  ███    ███ ███    ███   ███    ███ ███▄▄▄███
+ ▄███▄▄▄▄██▀ ███    ███  ▄███▄▄▄██▀  ▀▀▀▀▀▀███
+▀▀███▀▀▀▀▀   ███    ███ ▀▀███▀▀▀██▄  ▄██   ███
+▀███████████ ███    ███   ███    ██▄ ███   ███
+  ███    ███ ███    ███   ███    ███ ███   ███
+  ███    ███ ████████▀  ▄█████████▀   ▀█████▀
+  ███    ███
+"""
+
+print(earl)
+print(" A Rumble, Bitchute, and YouTube scraper\n")
+print(" By: Jake Creps \n")
+
+print(" [!] -- Search complete. Check search.csv -- [!]\n")
+
+with open('search.csv',"r") as f:
+    reader = csv.reader(f,delimiter = ",")
+    data = list(reader)
+    row_count = len(data)
+
+print(" [#] -- Ruby has collected " + str(row_count) + " videos -- [#]\n")
