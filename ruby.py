08198d2119505e10450074904367a830fdacdffb
Jake Creps <crepschrome@gmail.com>
2022-05-27 07:58:08 -0400

    Update ruby.py

--- ruby.py
+++ ruby.py
@@ -8,6 +8,7 @@ import os.path
 from scripts.bitchute import *
 from scripts.rumble import *
 from scripts.youtube import *
+import time
 
 init(autoreset=True)
 
@@ -24,14 +25,23 @@ ruby = Fore.RED + r"""
 """
 
 print(ruby)
-print(" A Rumble, Bitchute, and YouTube scraper\n")
+print(" A Rumble, Bitchute, and YouTube scraper and search engine \n")
 print(" By: Jake Creps \n")
+print(Fore.RED + " Ruby is searching... \n")
+time.sleep(2)
 
-print(" [!] -- Search complete. Check search.csv -- [!]\n")
+print(Fore.RED + " [!] -- Rumble results -- [!] \n")
+rumble()
+print(Fore.RED + " [!] -- BitChute results -- [!] \n")
+bitchute()
+print(Fore.RED + " [!] -- YouTube results -- [!] \n")
+youtube()
 
 with open('search.csv',"r") as f:
     reader = csv.reader(f,delimiter = ",")
     data = list(reader)
     row_count = len(data)
 
-print(" [#] -- Ruby has collected " + str(row_count) + " videos -- [#]\n")
+print(Fore.RED + " [!] -- Search complete. Check search.csv -- [!]\n")
+
+print(Fore.RED + " [#] -- Ruby has collected " + str(row_count) + " total videos -- [#]\n")
